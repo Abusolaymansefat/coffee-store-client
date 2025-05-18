dd871cc84ba2d7edc3f1b7429b435c2f4a4ce116
Abusolay <sefat01625@gmail.com>
2025-05-18 22:23:01 +0600

    users information

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const SignIn = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const handleSingIn= (e)=> {
+        e.preventDefault()
+    }
+  return (
+    <div className="card bg-base-100 mx-auto max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-5xl font-bold">Sign In now!</h1>
+        <form onSubmit={handleSingIn} className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            name="email"
+            className="input"
+            placeholder="Email"
+          />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            name="password"
+            className="input"
+            placeholder="Password"
+          />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">SingIn</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import CoffeeDetails from "./components/CoffeeDetails.jsx";
 import SignIn from "./components/SignIn.jsx";
 import SingUp from "./components/SingUp.jsx";
 import AuthProvider from "./contexts/AuthProvider.jsx";
+import Users from "./components/Users.jsx";
 
 const router = createBrowserRouter([
   {
@@ -45,6 +46,11 @@ const router = createBrowserRouter([
         path: "singup",
         Component: SingUp,
       },
+      {
+        path: 'users',
+        loader:() => fetch('http://localhost:3000/users'),
+        Component: Users,
+      }
     ],
   },
 ]);
